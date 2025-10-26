65e61f6b2773966337572d5456812c4abc9a4b7f
Triya <tbhupathiraju@gmail.com>
2025-10-26 13:58:47 -0400

    Debug, Summary, Visualization #1

--- economic_trends_viz.py
+++ economic_trends_viz.py
@@ -1,11 +1,13 @@
 # Imported necessary libraries
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime
 
 # Loaded the dataset
-df = pd.read_csv('T10YIE.csv')
+data_path = os.path.join(os.path.dirname(__file__), 'T10YIE.csv')
+df = pd.read_csv(data_path)
 
 #Inspect the data
 print(df.head())
@@ -14,8 +16,8 @@ print(df.info())
 # Data Cleaning
 #Renaming the columns
 df.columns = ["Date", "Rate"]
-#Converting the date column to datatime format (mm/dd/yyyy)
-df["Date"] = pd.to_datetime(df["Date"], format="%m/%d/%y")
+#Converting the date column to datatime format
+df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
 #Missing Values
 df["Rate"] = pd.to_numeric(df["Rate"], errors="coerce")
 #Removing Duplicates
@@ -23,5 +25,22 @@ df.drop_duplicates(inplace=True)
 df.dropna(inplace=True)
 #Sorting the data by date
 df.sort_values(by="Date", inplace=True)
+#Sequential Indexing
+df.reset_index(drop=True, inplace=True)
+print(df.info())
 
+#Statistical Summary
+print("Statistical Summary")
+print(df.describe())
+
+#Data Visualizations
+#Visualization - 1
+plt.figure(figsize=(12,6))
+plt.plot(df["Date"], df["Rate"], color="navy", linewidth=1)
+plt.title("10-Year Inflation Expectation Rate Over Time", fontsize=16)
+plt.xlabel("Year", fontsize=14)
+plt.ylabel("Inflation Expectation Rate (%)", fontsize=14)
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.tight_layout()
+plt.show()
 
