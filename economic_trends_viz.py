f1fa222807d75c9875057997103d015c0cc5550c
Triya <tbhupathiraju@gmail.com>
2025-10-26 13:00:26 -0400

    Cleaning Data

--- economic_trends_viz.py
+++ economic_trends_viz.py
@@ -1,8 +1,27 @@
+# Imported necessary libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from datatime import datetime
+from datetime import datetime
 
+# Loaded the dataset
 df = pd.read_csv('T10YIE.csv')
+
+#Inspect the data
+print(df.head())
+print(df.info())
+
+# Data Cleaning
+#Renaming the columns
+df.columns = ["Date", "Rate"]
+#Converting the date column to datatime format (mm/dd/yyyy)
+df["Date"] = pd.to_datetime(df["Date"], format="%m/%d/%y")
+#Missing Values
+df["Rate"] = pd.to_numeric(df["Rate"], errors="coerce")
+#Removing Duplicates
+df.drop_duplicates(inplace=True)
+df.dropna(inplace=True)
+#Sorting the data by date
+df.sort_values(by="Date", inplace=True)
 
 
